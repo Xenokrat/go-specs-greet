1ac895c121922a934b50cbcb263a8a6f03e12919
Pavel Barabanov <barabanovpv95@gmail.com>
2025-08-11 13:19:37 +0300

    refactor: consilidate Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@ FROM golang:1.24.5-alpine
 
 WORKDIR /app
 
+ARG bin_to_build
+
 COPY go.mod ./
 
 RUN go mod download
 
 COPY . .
 
-RUN go build -o svr cmd/grpcserver/*.go
+RUN go build -o svr cmd/${bin_to_build}/*.go
 
-EXPOSE 50051
 CMD [ "./svr" ]
--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -7,20 +7,19 @@ import (
 	"time"
 
 	"github.com/Xenokrat/go-specs-greet/adapters"
-	"github.com/Xenokrat/go-specs-greet/specifications"
 	"github.com/Xenokrat/go-specs-greet/adapters/httpserver"
+	"github.com/Xenokrat/go-specs-greet/specifications"
 )
 
 func TestGreeterServer(t *testing.T) {
 	var (
-		port = "8080"
-		dockerFilePath = "./cmd/httpserver/Dockerfile"
+		port    = "8080"
 		baseURL = fmt.Sprintf("http://localhost:%s", port)
-		driver = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{
+		driver  = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{
 			Timeout: 1 * time.Second,
 		}}
 	)
 
-		adapters.StartDockerServer(t, port, dockerFilePath)
-		specifications.GreetSpecification(t, driver)
+	adapters.StartDockerServer(t, port, "httpserver")
+	specifications.GreetSpecification(t, driver)
 }
